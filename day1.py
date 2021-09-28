8934cfb0b6e78e0c336b5865b210d18ce145b2cf
Sabbir Ahmed <38248492+sabbir1021@users.noreply.github.com>
2021-09-28 13:01:05 +0600

    1st commit

---
+++ day1.py
@@ -0,0 +1,17 @@
+
+# Declare second integer, double, and String variables.
+i2 = int(input())
+d2 = float(input())
+s2 = str(input())
+
+# Read and save an integer, double, and String to your variables.
+i_sum = i+i2
+d_sum = d+d2
+s_sum = s+s2
+# Print the sum of both integer variables on a new line.
+print(i_sum)
+# Print the sum of the double variables on a new line.
+print(d_sum)
+# Concatenate and print the String variables on a new line
+print(s_sum)
+# The 's' variable above should be printed first.
